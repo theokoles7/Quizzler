327042fc34c93b01884ee78f2adce85ce39b5319
theokoles <gabrieltrahan777@hotmail.com>
2022-01-06 14:20:25 -0600

    Initialized components

--- ui/src/app/modules/views/views.module.ts
+++ ui/src/app/modules/views/views.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu/menu.component';
+import { GameComponent } from './game/game.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MenuComponent,
+    GameComponent
+  ],
   imports: [
     CommonModule
   ]
