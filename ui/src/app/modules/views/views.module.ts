ca7dc361d9864761ff7a307ba73ddd013919bb93
theokoles <gabrieltrahan777@hotmail.com>
2022-01-07 11:11:41 -0600

    Adds page for view of license

--- ui/src/app/modules/views/views.module.ts
+++ ui/src/app/modules/views/views.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { GameComponent } from './game/game.component';
+import { LicenseComponent } from './license/license.component';
 
 
 
 @NgModule({
   declarations: [
     MenuComponent,
-    GameComponent
+    GameComponent,
+    LicenseComponent
   ],
   imports: [
     CommonModule
