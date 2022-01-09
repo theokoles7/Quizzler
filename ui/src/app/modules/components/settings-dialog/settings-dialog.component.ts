5f58fbd8d5fbb83ed295da306a5397b5eecaf0d2
theokoles <gabrieltrahan777@hotmail.com>
2022-01-09 12:16:52 -0600

    Creates Settings Dialog component

---
+++ ui/src/app/modules/components/settings-dialog/settings-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'q-settings-dialog',
+  templateUrl: './settings-dialog.component.html',
+  styleUrls: ['./settings-dialog.component.scss']
+})
+export class SettingsDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
