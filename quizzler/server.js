32c5b4e563273fcbe6a0ed2052f30798f8c1020b
theokoles <gabrieltrahan777@hotmail.com>
2022-01-17 12:52:40 -0600

    Changes for prod

--- quizzler/server.js
+++ quizzler/server.js
@@ -1,10 +1,14 @@
-function requireHTTPS(req, res, next){
-  if(!req.secure && req.get('x-forwarded-proto') !== 'https'){
-    return res.redirect('https://' + req.get('host') + req.url);
+console.log("I am running");
+
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
   }
   next();
 }
 
+const { on } = require('events');
 const express = require('express');
 const app = express();
 app.use(requireHTTPS);
