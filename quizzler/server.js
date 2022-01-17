63dd26991b7b0d3d0487beaab680849ef3c00abf
theokoles <gabrieltrahan777@hotmail.com>
2022-01-17 11:38:56 -0600

    Creates server.js

---
+++ quizzler/server.js
@@ -0,0 +1,19 @@
+function requireHTTPS(req, res, next){
+  if(!req.secure && req.get('x-forwarded-proto') !== 'https'){
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+const express = require('express');
+const app = express();
+app.use(requireHTTPS);
+
+app.use(express.static(`./dist/quizzler`));
+
+app.get(`/*`, function(req, res){
+  res.sendFile(`index.html`, {root: 'dist/quizzler/'}
+  );
+});
+
+app.listen(process.env.PORT || 8080);
